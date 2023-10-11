53d8fa9dc6dfb13b51cf2ebdf0a64f20bb824df1
MikiBersa <trombettamiki@gmail.com>
2023-10-11 21:55:29 +0200

    Done Task1: 1) getDataPoint 2) getRatio 3) Unit tests

--- client3.py
+++ client3.py
@@ -31,18 +31,20 @@ N = 500
 
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    # MEDIAN OF THE PRICE BETWEEN BID and ASk
+    price = (ask_price + bid_price) / 2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
+    if price_b == 0:
+        # PARTICULAR CASE, WE MUST THINK ALL THE POSSIBLE CASE
+        return
+    return price_a / price_b
 
 
 # Main
@@ -51,9 +53,12 @@ if __name__ == "__main__":
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        """ ----------- Update to get the ratio --------------- """
+        # CREATION OF A DICTIONARY TO STORE THE DATA OF THE PRICE
+        price = {}
         for quote in quotes:
-            stock, bid_price, ask_price, price = getDataPoint(quote)
+            # I'm going to read the two items
+            stock, bid_price, ask_price, price[stock] = getDataPoint(quote)
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        print("Ratio %s" % getRatio(price["ABC"], price["DEF"]))
+
